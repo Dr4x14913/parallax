7d64537bfddb8818fb75d31ea46cc5002ddaee32
Nathan <dr4x.14913@gmail.com>
2025-12-24 18:54:28 +0100

    feat: add mobile bg choice in admin pane

--- functions.php
+++ functions.php
@@ -117,6 +117,51 @@ function alterAdmin($args) {
 
     $doc->getElementById("currentPage")->insertBefore($form_group, $doc->getElementById("currentPage")->lastChild->previousSibling->previousSibling);
 
+    /* Input element for mobile background */
+
+    $label = $doc->createElement("p");
+    $label->setAttribute("class", "subTitle");
+    $label->nodeValue = "Mobile background image";
+
+    $doc->getElementById("currentPage")->insertBefore($label, $doc->getElementById("currentPage")->lastChild->previousSibling->previousSibling);
+
+	$form_group = $doc->createElement("div");
+    $form_group->setAttribute("class", "form-group");
+
+    $wrapper = $doc->createElement("div");
+    $wrapper->setAttribute("class", "change");
+
+    $input = $doc->createElement("select");
+    $input->setAttribute("class", "form-control");
+    $input->setAttribute("onchange", "fieldSaveParallax('mobileBackground',this.value,'pages');");
+    $input->setAttribute("name", "mobileBackgroundSelect");
+
+	$option = $doc->createElement("option");
+	$option->setAttribute("value", "");
+	$option->nodeValue = "Theme default";
+	$input->appendChild($option);
+
+	$files = glob($Wcms->filesPath . "/*");
+	foreach($files as $file) {
+		if(!in_array(getimagesize($file)[2], array(IMAGETYPE_GIF, IMAGETYPE_JPEG, IMAGETYPE_PNG, IMAGETYPE_BMP))) continue;
+
+		$file = basename($file);
+
+		$option = $doc->createElement("option");
+	    $option->setAttribute("value", $file);
+		$option->nodeValue = $file;
+
+		if(isset($Wcms->get("pages", $Wcms->currentPage)->mobileBackground) && $Wcms->get("pages", $Wcms->currentPage)->mobileBackground == $file)
+			$option->setAttribute("selected", "selected");
+
+		$input->appendChild($option);
+	}
+
+    $wrapper->appendChild($input);
+    $form_group->appendChild($wrapper);
+
+    $doc->getElementById("currentPage")->insertBefore($form_group, $doc->getElementById("currentPage")->lastChild->previousSibling->previousSibling);
+
     /* Input element for parallax type */
 
     $label = $doc->createElement("p");
@@ -198,6 +243,7 @@ function alterAdmin($args) {
     $args[0] = preg_replace('~<(?:!DOCTYPE|/?(?:html|body))[^>]*>\s*~i', '', $doc->saveHTML());
     return $args;
 }
+
 $Wcms->addListener('settings', 'alterAdmin');
 
 ?>
--- theme.php
+++ theme.php
@@ -3,6 +3,7 @@
 global $Wcms;
 
 $page_image = $Wcms->asset('images/default.jpg');
+$mobile_page_image = null;
 $heading = $Wcms->page('title');
 
 $height = 100;
@@ -14,22 +15,24 @@ $logo = "";
 if($Wcms->currentPage == $Wcms->get('config', 'login')) {
 	$subtitle = $Wcms->page('content');
 } else {
-	// Check if image for this page exists
+	// Desktop background
 	if(isset($Wcms->get("pages", $Wcms->currentPage)->background) && $Wcms->get("pages", $Wcms->currentPage)->background != "") {
 		$page_image = "data/files/" . $Wcms->get("pages", $Wcms->currentPage)->background;
 	}
 
-	$height = isset($Wcms->get('pages', $Wcms->currentPage)->themeHeaderHeight) ? $Wcms->get('pages', $Wcms->currentPage)->themeHeaderHeight : 100;
+	// Mobile background (per-page setting)
+	if(isset($Wcms->get("pages", $Wcms->currentPage)->mobileBackground) && $Wcms->get("pages", $Wcms->currentPage)->mobileBackground != "") {
+		$mobile_page_image = "data/files/" . $Wcms->get("pages", $Wcms->currentPage)->mobileBackground;
+	}
 
-	// Get logo from page settings if set, otherwise use default
+	$height = isset($Wcms->get('pages', $Wcms->currentPage)->themeHeaderHeight) ? (int)$Wcms->get('pages', $Wcms->currentPage)->themeHeaderHeight : 100;
+
+	// Logo
 	if($Wcms->get("config", "logo") !== null && $Wcms->get("config", "logo") != "" && !is_object($Wcms->get("config", "logo"))) {
 		$logo = "data/files/" . $Wcms->get("config", "logo");
 	} else {
 		$logo = $Wcms->asset('images/default.jpg');
 	}
-
-	// $heading = getEditableArea("heading", $Wcms->page('title'));
-	// $subtitle = getEditableArea("subtitle", "");
 
 	$type = isset($Wcms->get('pages', $Wcms->currentPage)->parallax) ? $Wcms->get('pages', $Wcms->currentPage)->parallax : "dual";
 	$readMore = isset($Wcms->get('pages', $Wcms->currentPage)->readMoreText) ? $Wcms->get('pages', $Wcms->currentPage)->readMoreText : "Read more";
@@ -43,7 +46,6 @@ if($Wcms->currentPage == $Wcms->get('config', 'login')) {
 -->
 <html lang="en">
 <head>
-
 	<meta charset="UTF-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
@@ -60,15 +62,36 @@ if($Wcms->currentPage == $Wcms->get('config', 'login')) {
 	<!-- Theme CSS -->
 	<link rel="stylesheet" href="<?= $Wcms->asset('css/style.css') ?>">
 
-	<?php /* Translate php to some javascript variables and css rules. Please don't replicate this. */ ?>
-	<script>var page=<?=json_encode($Wcms->currentPage)?>,heading=<?=json_encode($heading)?>,subtitle=<?=json_encode($subtitle)?>,image=<?=json_encode($page_image)?>,height=<?=json_encode($height)?>,type=<?=json_encode($type)?>,loggedIn=<?=json_encode($Wcms->loggedIn)?>;</script>
-	<style>.parallax{height:<?=htmlentities($height)?>vh;}</style>
+	<?php /* Pass PHP vars to JS */ ?>
+	<script>
+		var page=<?=json_encode($Wcms->currentPage)?>,
+			heading=<?=json_encode($heading)?>,
+			subtitle=<?=json_encode($subtitle)?>,
+			image=<?=json_encode($page_image)?>,
+			height=<?=json_encode($height)?>,
+			type=<?=json_encode($type)?>,
+			loggedIn=<?=json_encode($Wcms->loggedIn)?>;
+	</script>
+
+	<style>
+		.parallax {
+			height: <?= (int)$height ?>vh;
+			background-image: url(<?= json_encode($page_image) ?>);
+		}
+		<?php if ($mobile_page_image): ?>
+			@media (max-width: 768px) {
+				.parallax {
+					background-image: url(<?= json_encode($mobile_page_image) ?>);
+				}
+			}
+		<?php endif; ?>
+	</style>
 </head>
 <body>
-		<?= $Wcms->settings() ?>
-		<?= $Wcms->alerts() ?>
+	<?= $Wcms->settings() ?>
+	<?= $Wcms->alerts() ?>
 
-	<nav class="navbar navbar-default<?php if($height == 0)echo " background"; if(!$Wcms->loggedIn && $height == 0)echo " sticky no-animation"; ?>">
+	<nav class="navbar navbar-default<?php if($height == 0) echo " background"; if(!$Wcms->loggedIn && $height == 0) echo " sticky no-animation"; ?>">
 		<div class="container">
 			<div class="navbar-header">
 				<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
@@ -82,7 +105,7 @@ if($Wcms->currentPage == $Wcms->get('config', 'login')) {
 				</a>
 			</div>
 			<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-	  			<ul class="nav navbar-nav navbar-right">
+				<ul class="nav navbar-nav navbar-right">
 					<?=$Wcms->menu()?>
 				</ul>
 			</div>
@@ -90,24 +113,24 @@ if($Wcms->currentPage == $Wcms->get('config', 'login')) {
 	</nav>
 
 	<?php if($height != 0): ?>
-
 	<header class="parallax-wrapper">
-		<div class="parallax" style='background-image: url(<?=json_encode($page_image)?>);'>
+		<div class="parallax">
 			<div class="inner">
 				<h1><?= $heading ?></h1>
 				<?= $Wcms->loggedIn ? $subtitle : "<p>$subtitle</p>" ?>
 			</div>
 			<?php if($readMore): ?>
 			<a href="#content" class="scrolly"><?=$readMore?><br>
-			<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 512"><path d="M119.5 262.9L3.5 145.1c-4.7-4.7-4.7-12.3 0-17l7.1-7.1c4.7-4.7 12.3-4.7 17 0L128 223.3l100.4-102.2c4.7-4.7 12.3-4.7 17 0l7.1 7.1c4.7 4.7 4.7 12.3 0 17L136.5 263c-4.7 4.6-12.3 4.6-17-.1zm17 128l116-117.8c4.7-4.7 4.7-12.3 0-17l-7.1-7.1c-4.7-4.7-12.3-4.7-17 0L128 351.3 27.6 249.1c-4.7-4.7-12.3-4.7-17 0l-7.1 7.1c-4.7 4.7-4.7 12.3 0 17l116 117.8c4.7 4.6 12.3 4.6 17-.1z" fill="#ffffff"/></svg></a>
+				<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 512">
+					<path d="M119.5 262.9L3.5 145.1c-4.7-4.7-4.7-12.3 0-17l7.1-7.1c4.7-4.7 12.3-4.7 17 0L128 223.3l100.4-102.2c4.7-4.7 12.3-4.7 17 0l7.1 7.1c4.7 4.7 4.7 12.3 0 17L136.5 263c-4.7 4.6-12.3 4.6-17-.1zm17 128l116-117.8c4.7-4.7 4.7-12.3 0-17l-7.1-7.1c-4.7-4.7-12.3-4.7-17 0L128 351.3 27.6 249.1c-4.7-4.7-12.3-4.7-17 0l-7.1 7.1c-4.7 4.7-4.7 12.3 0 17l116 117.8c4.7 4.6 12.3 4.6 17-.1z" fill="#ffffff"/>
+				</svg>
+			</a>
 			<?php endif; ?>
 		</div>
 	</header>
-
 	<?php endif; ?>
 
 	<?php if($Wcms->currentPage != $Wcms->get('config', 'login')): ?>
-
 	<div class="" id="content">
 		<div class="row">
 			<div class="col-lg-12 text-center padding40">
@@ -117,15 +140,12 @@ if($Wcms->currentPage == $Wcms->get('config', 'login')) {
 	</div>
 
 	<div class="container-fluid CTA">
-			<div class="text-center padding40">
-				<?=$Wcms->block('subside')?>
-			</div>
+		<div class="text-center padding40">
+			<?=$Wcms->block('subside')?>
+		</div>
 	</div>
-
 	<?php else: ?>
-
 	<style>.parallax .scrolly { display: none }</style>
-
 	<?php endif; ?>
 
 	<footer class="container-fluid">
@@ -135,14 +155,17 @@ if($Wcms->currentPage == $Wcms->get('config', 'login')) {
 			<br><br><br>
 		</div>
 	</footer>
-	<script src='https://code.jquery.com/jquery-1.12.4.min.js' integrity='sha384-nvAa0+6Qg9clwYCGGPpDQLVpLNn0fRaROjHqs13t4Ggj3Ez50XnGQqc/r8MhnRDZ' crossorigin='anonymous'></script>
+
+	<script src="https://code.jquery.com/jquery-1.12.4.min.js" integrity="sha384-nvAa0+6Qg9clwYCGGPpDQLVpLNn0fRaROjHqs13t4Ggj3Ez50XnGQqc/r8MhnRDZ" crossorigin="anonymous"></script>
 	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha256-U5ZEeKfGNOja007MMD3YBI0A3OSZOQbeG6z2f2Y0hu8=" crossorigin="anonymous"></script>
 	<?=$Wcms->js()?>
-
 	<script src="<?=$Wcms->asset('js/script.js')?>"></script>
 
-	<?php /* Add some overwrites for pages with different parallax types. */ ?>
-	<?php if($type == "scroll") echo "<style>.parallax{background-attachment:fixed;}</style>"; ?>
-	<?php if($height == 100) echo "<style>.parallax{padding-top:0;}</style>"; ?>
+	<?php if($type == "scroll"): ?>
+		<style>.parallax{background-attachment:fixed;}</style>
+	<?php endif; ?>
+	<?php if($height == 100): ?>
+		<style>.parallax{padding-top:0;}</style>
+	<?php endif; ?>
 </body>
 </html>
